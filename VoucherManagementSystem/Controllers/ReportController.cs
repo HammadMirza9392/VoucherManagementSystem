912b03044dac3fcce7445e48c7effb65c83a777a
Hammad Mirza <hammadhassanmirzasiddique@gmail.com>
2026-06-04 16:10:13 +0500

    added the porject column inthe cash report

--- VoucherManagementSystem/Controllers/ReportController.cs
+++ VoucherManagementSystem/Controllers/ReportController.cs
@@ -288,6 +288,7 @@ namespace VoucherManagementSystem.Controllers
                     .Include(v => v.ReceivingCustomer)
                     .Include(v => v.Item)
                     .Include(v => v.ExpenseHead)
+                    .Include(v => v.Project)
                     .Where(v => v.CashType == CashType.Cash &&
                                v.VoucherDate >= startDate && v.VoucherDate <= endDate.AddDays(1));
 
